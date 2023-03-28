78eb46c73705e30d641bfa43568350bc6e26c4d8
Sridhar R <sridharofficialmail@gmail.com>
2023-03-28 17:14:31 +0530

    signup

--- src/user/Service/user.ts
+++ src/user/Service/user.ts
@@ -7,7 +7,7 @@ import user, { IUser } from "../Model/user";
 export class UserService {
   public async userSignUp(data: IUser): Promise<IUser | any> {
     const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(data.password, salt);
+    data.password = await bcrypt.hash(data.password, salt);
     return new Promise<IUser | any>(async (resolve, reject) => {
       try {
         const Data = new user(data);
